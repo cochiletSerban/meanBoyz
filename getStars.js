66bf1ae5ee7953dcffae215356ab2ebb71a05c74
darkriderserban <cochiletserban@gmail.com>
2018-04-08 11:03:41 +0000

    make it better

---
+++ getStars.js
@@ -0,0 +1,31 @@
+
+const googleImages = require("google-images");
+const request = require("request-promise-native");
+const _ = require('lodash');
+
+const client = new googleImages('014687730034597538767:cssm6aogojs','AIzaSyAeiMksjWcX26bqvJorGZfGNaARgbjS5MU');
+const url = "https://www.pornhub.com/webmasters/stars_detailed";
+
+module.exports.getDb = async function getDb() {
+    let response  = await request.get({url,json:true});
+    let newStars = [];
+    for (let i=0;i<response.stars.length;i++)
+        if (response.stars[i].star.videos_count_all > 250 && response.stars[i].star.gender=="female")
+            newStars.push(response.stars[i].star.star_name); 
+    return newStars;
+};
+
+module.exports.getStar = async function getStar(stars, mode) {
+    let i = Math.floor(Math.random()*stars.length);
+    let name = stars[i];
+    if(mode == undefined) {
+        let q = `${name}  pornstar lingerie `;
+        let imageUrl = await client.search(q, {size:'xlarge'});
+        let pornStar = {
+            name: name,
+            url: [imageUrl[0].url, imageUrl[1].url, imageUrl[2].url]
+        }
+        return pornStar;
+    } else 
+        return {name};
+};
