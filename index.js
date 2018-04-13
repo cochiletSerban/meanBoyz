e81e26da944485a460476aa3965fa909d0c4a39a
darkriderserban <cochiletserban@gmail.com>
2018-04-13 21:40:28 +0000

    to be replace

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict'
+const getStar = require("./getStars.js");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const port = process.env.PORT || 4200;
+
+let db; 
+const app = express();
+
+async function sendStar(db) {
+    let pornstar = await getStar.getStar(db);
+    console.log("res is: " + pornstar.name);
+    return pornstar;
+}
+
+getStar.getDb().then(data => {
+    console.log("ready");
+    db = data;
+    app.get('/getStar', (req, res) => {
+        console.log("got req");
+        sendStar(db)
+            .then(pornstar => res.send(pornstar))
+            .catch(err => console.log(err));
+    })
+}).catch(err => console.log("cant init db"));
+
+
+app.listen(port,() => {
+    console.log("started @ " + port);
+});
+
