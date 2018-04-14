c2b03f28db80d1567f162d3eca3dde5054eb37cc
AdrianStefan23 <adrian.cosmin23@gmail.com>
2018-04-14 15:14:26 +0300

    intorcNume

--- test.js
+++ test.js
@@ -9,13 +9,44 @@ let app = express();
 app.use(bodyParser.json());
 
 app.post('/mizerii', (req, res) => {
-	let mesaj = {
+	
+	// console.log(req.body.tip);
+	// console.log(req.body.address);
+	// console.log(req.body.types);
+	// console.log(req.body.radius);
+	// console.log(req.body.name);
+	//let numeRestaurant;
 
-		text: req.body.text
-	};
+	if (req.body.tip) {
+		let encoded_addr = encodeURIComponent(req.body.address);
+		let geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encoded_addr}&key=AIzaSyD338tPvWw4Oot4eQZ2UNukpjMzyo5jVWM`
+		axios.get(geocodeUrl).then((response) => {
+		if (response.data.status === 'ZERO_RESULTS') {
+			throw new Error('Unable to find that address');
+		}
+		let lat = response.data.results[0].geometry.location.lat;
+		let lng = response.data.results[0].geometry.location.lng;
+		let radius = req.body.radius;
+		let types = req.body.types;
+		let name = req.body.name;
 
-	res.send(mesaj);
-
+		//console.log(lat, lng, radius, types, name);
+		let placesUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=${radius}&types=${types}&name=${name}&key=AIzaSyD338tPvWw4Oot4eQZ2UNukpjMzyo5jVWM`;
+		//console.log("URL: ", placesUrl);
+		return axios.get(placesUrl);
+		}).then((response) => {
+			if (response.data.status === 'ZERO_RESULTS') {
+				res.send('No matches found');
+			}
+			res.send(response.data.results[0].name);
+		}).catch((e) => {
+			if (e.code === 'ENOTFOUND') {
+				console.log('Unable to connect to API servers');
+			} else {
+				console.log(e.message);
+			}
+		});
+	}
 });
 
 app.listen(port, () => {
