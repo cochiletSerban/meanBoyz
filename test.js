b3b4e9dc4e38c918061a71c631d58fee13b857a2
AdrianStefan23 <adrian.cosmin23@gmail.com>
2018-04-14 17:03:37 +0300

    postGetDemo

--- test.js
+++ test.js
@@ -10,13 +10,6 @@ app.use(bodyParser.json());
 
 app.post('/mizerii', (req, res) => {
 	
-	// console.log(req.body.tip);
-	// console.log(req.body.address);
-	// console.log(req.body.types);
-	// console.log(req.body.radius);
-	// console.log(req.body.name);
-	//let numeRestaurant;
-
 	if (req.body.tip) {
 		let encoded_addr = encodeURIComponent(req.body.address);
 		let geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encoded_addr}&key=AIzaSyD338tPvWw4Oot4eQZ2UNukpjMzyo5jVWM`
@@ -35,45 +28,75 @@ app.post('/mizerii', (req, res) => {
 		//console.log("URL: ", placesUrl);
 		return axios.get(placesUrl);
 		}).then((response) => {
+			let listRet = [];
 			if (response.data.status === 'ZERO_RESULTS') {
 				res.send('No matches found');
 			}
-			res.send(response.data.results[0].name);
+			
+			response.data.results.forEach((result) => {
+				//console.log(result);
+				let locatie = [];
+				//console.log(result.geometry.location);
+				locatie.push(result.geometry.location.lat);
+				locatie.push(result.geometry.location.lng);
+				let obj = {
+					name: result.name,
+					location: locatie
+				};
+				listRet.push(obj);
+			});
+			res.send(listRet);
 		}).catch((e) => {
 			if (e.code === 'ENOTFOUND') {
-				console.log('Unable to connect to API servers');
+				res.send('Unable to connect to API servers');
 			} else {
-				console.log(e.message);
+				res.send(e.message);
+			}
+		});
+	} else {
+
+		let lat = req.body.location.lat;
+		let lng = req.body.location.lng;
+		let radius = req.body.radius;
+		let types = req.body.types;
+		let name = req.body.name;
+
+		//console.log(lat, lng, radius, types, name);
+		let placesUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=${radius}&types=${types}&name=${name}&key=AIzaSyD338tPvWw4Oot4eQZ2UNukpjMzyo5jVWM`;
+		//console.log("URL: ", placesUrl);
+		return axios.get(placesUrl).then((response) => {
+			let listRet = [];
+			if (response.data.status === 'ZERO_RESULTS') {
+				res.send('No matches found');
+			}
+			
+			response.data.results.forEach((result) => {
+				//console.log(result);
+				let locatie = [];
+				//console.log(result.geometry.location);
+				locatie.push(result.geometry.location.lat);
+				locatie.push(result.geometry.location.lng);
+				let obj = {
+					name: result.name,
+					location: locatie
+				};
+				listRet.push(obj);
+			});
+			res.send(listRet);
+		}).catch((e) => {
+			if (e.code === 'ENOTFOUND') {
+				res.send('Unable to connect to API servers');
+			} else {
+				res.send(e.message);
 			}
 		});
 	}
 });
 
+app.get('/cacat', (req, res) => {
+	res.send('Ia ba niste pula de la sefu tau');
+});
+
 app.listen(port, () => {
 	console.log('Trimiteti fratii mei pe portul 3000');
 });
-
-// let encoded_addr = encodeURIComponent(argv.address);
-
-// let geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encoded_addr}&key=AIzaSyD338tPvWw4Oot4eQZ2UNukpjMzyo5jVWM`
-
-// axios.get(geocodeUrl).then((response) => {
-// 	if (response.data.status === 'ZERO_RESULTS') {
-// 		throw new Error('Unable to find that address');
-// 	}
-// 	let lat = response.data.results[0].geometry.location.lat;
-// 	let lng = response.data.results[0].geometry.location.lng;
-// 	let weatherUrl = `https://api.darksky.net/forecast/168b93124cf6a91acdb4e172d0226021/${lat},${lng}`;
-// 	console.log(response.data.results[0].formatted_address);
-// 	return axios.get(weatherUrl);
-// }).then((response) => {
-// 	let temperature = response.data.currently.temperature;
-// 	let apparentTemperature = response.data.currently.apparentTemperature;
-// 	console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}`);
-// }).catch((e) => {
-// 	if (e.code === 'ENOTFOUND') {
-// 		console.log('Unable to connect to API servers');
-// 	} else {
-// 		console.log(e.message);
-// 	}
-// });
